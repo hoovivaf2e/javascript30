86fba035796f09ceb16b06dba028970b60a8d0c4
Eileen <hooviva77@gmail.com>
2019-06-26 12:22:27 +0800

    modify javascript 30: 1/30

--- 01_ Drum_Kit/js/script.js
+++ 01_ Drum_Kit/js/script.js
@@ -2,6 +2,7 @@ function removeTransition(e) {
   if (e.propertyName !== 'transform') return;
   e.target.classList.remove('playing');
 }
+
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
@@ -10,6 +11,7 @@ function playSound(e) {
   audio.currentTime = 0;
   audio.play();
 }
+
 const keys = Array.from(document.querySelectorAll('.key'));
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
